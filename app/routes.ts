0b481ef066c477b235f95ba15011125cf9b4aaa5
Galuh Wasesa <surat.sesha@gmail.com>
2025-08-19 18:52:49 +0700

    feat : product by slug route

--- app/routes.ts
+++ app/routes.ts
@@ -9,6 +9,7 @@ export default [
   layout("./layouts/layout-main.tsx", [
     index("routes/home.tsx"),
     route("products", "./routes/products.tsx"),
+    route("products/:slug", "./routes/products-slug.tsx"),
     // ...
   ]),
 ] satisfies RouteConfig;
---
+++ app/routes/products-slug.tsx
@@ -0,0 +1,25 @@
+import type { Route } from "./+types/products-slug";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "(Product Name) of Seliya" }];
+}
+
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/products/${params.slug}`
+  );
+  const product = await response.json();
+  return product;
+}
+
+export default function ProductsRoute({ loaderData }: Route.ComponentProps) {
+  const { product } = loaderData;
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <img src={product.imageUrl} alt={product.name} />
+      <p>{product.description}</p>
+    </div>
+  );
+}
