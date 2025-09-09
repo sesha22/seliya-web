b62b9dbec9bd08774e56550946d0de9282f81647
Galuh Wasesa <surat.sesha@gmail.com>
2025-09-09 09:33:41 +0700

    feat ; register login form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,8 @@
+import { Form } from "react-router";
 import type { Route } from "./+types/login";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Login" }];
@@ -6,8 +10,41 @@ export function meta({}: Route.MetaArgs) {
 
 export default function LoginRoute({}: Route.ComponentProps) {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="mx-auto w-full max-w-md py-6">
+      <header className="space-y-1">
+        <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+        <p className="text-sm text-muted-foreground">
+          Continue to Your Account
+        </p>
+      </header>
+
+      <Form method="post" className=" space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="fullName">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+          />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="fullName">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            required
+          />
+        </div>
+
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,13 +1,85 @@
 import type { Route } from "./+types/register";
+import { Form, redirect } from "react-router";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Register" }];
 }
 
+export async function clientAction({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+
+  const fullName = formData.get("fullName");
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const registerBody = {
+    fullName,
+    email,
+    password,
+  };
+
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/register`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(registerBody),
+    }
+  );
+  const result = await response.json();
+
+  return redirect("/login");
+}
+
 export default function RegisterRoute({}: Route.ComponentProps) {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="mx-auto w-full max-w-md py-6">
+      <header className="space-y-1">
+        <h1 className="text-2xl font-semibold tracking-tight">Register</h1>
+        <p className="text-sm text-muted-foreground">Create Your Account</p>
+      </header>
+
+      <Form method="post" className=" space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="fullName">Full Name</Label>
+          <Input
+            id="fullName"
+            name="fullName"
+            type="text"
+            autoComplete="name"
+            required
+          />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="fullName">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+          />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="fullName">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            required
+          />
+        </div>
+
+        <Button type="submit" className="w-full">
+          Register
+        </Button>
+      </Form>
     </div>
   );
 }
