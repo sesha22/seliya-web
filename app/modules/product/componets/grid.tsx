7164bbac8c890f3a1749d78c7e163cf81fe3ed7c
Galuh Wasesa <surat.sesha@gmail.com>
2025-09-11 10:37:02 +0700

    feat : improve products grid UI

--- app/modules/product/componets/grid.tsx
+++ app/modules/product/componets/grid.tsx
@@ -4,19 +4,31 @@ import { Link } from "react-router";
 
 export function ProductsGrid({ products }: { products: Product[] }) {
   return (
-    <ul className="grid-cols-4 grid gap-4">
+    <ul
+      className="grid grid-cols-4 gap-6 p-4 mx-auto"
+      style={{ maxWidth: "900px" }}
+    >
       {products.map((product) => {
         return (
-          <li key={product.id}>
-            <Link to={`/products/${product.slug}`}>
-              <Card>
-                <img
-                  src={product.imageUrl}
-                  alt={product.name}
-                  className="size-40 object-cover"
-                />
-                <h4>{product.name}</h4>
-                <p>{product.price}</p>
+          <li key={product.id} className="transition-transform hover:scale-105">
+            <Link to={`/products/${product.slug}`} className="block h-full">
+              <Card className="flex flex-col items-center p-4 rounded-xl bg-white">
+                <div className="relative">
+                  <img
+                    src={product.imageUrl}
+                    alt={product.name}
+                    className="w-40 h-40 object-cover rounded-lg mb-4"
+                    style={{ aspectRatio: "1 / 1" }}
+                  />
+                </div>
+                <div className="py-4 space-y-2">
+                  <h4 className="text-md font-semibold text-gray-800 mb-2 text-center truncate w-full">
+                    {product.name}
+                  </h4>
+                  <p className="text-base font-bold text-indigo-600 mb-2 text-center">
+                    Rp {product.price.toLocaleString()}
+                  </p>
+                </div>
               </Card>
             </Link>
           </li>
